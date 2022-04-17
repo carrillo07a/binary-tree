bea6145958701e61a3b6e08f6eb0e8379bcab683
Aurelio Carrillo <carrillo07a@gmail.com>
2022-04-17 01:58:24 -0500

    Binary Tree Java

---
+++ src/tree.java
@@ -0,0 +1,26 @@
+public class tree {
+    public static void main(String[] args) {
+        BinaryTrees binaryTrees = new BinaryTrees();
+        binaryTrees.add(6);
+        binaryTrees.add(4);
+        binaryTrees.add(8);
+        binaryTrees.add(3);
+        binaryTrees.add(5);
+        binaryTrees.add(7);
+        binaryTrees.add(9);
+
+        binaryTrees.traverseInOrder(binaryTrees.root);
+        System.out.println("\n");
+        binaryTrees.traversePreOrder(binaryTrees.root);
+        System.out.println("\n");
+        binaryTrees.traversePostOrder(binaryTrees.root);
+        System.out.println("\n");
+        binaryTrees.traverseLevelOrder();
+        System.out.println("\n");
+        binaryTrees.traverseInOrderWithoutRecursion();
+        System.out.println("\n");
+        binaryTrees.traversePostOrderWithoutRecursion();
+        System.out.println("\n");
+        binaryTrees.traversePreOrderWithoutRecursion();
+    }
+}
